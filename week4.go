e7eda7a1e81caa9141f408e1a53ee14386261228
aarrcchh <65911896+aarrcchh@users.noreply.github.com>
2020-05-27 18:49:51 +0530

    Add files via upload

---
+++ week4.go
@@ -0,0 +1,74 @@
+
+/*
+Implement the dining philosopher’s problem with the following constraints/modifications.
+
+There should be 5 philosophers sharing chopsticks, with one chopstick between each adjacent pair of philosophers.
+Each philosopher should eat only 3 times (not in an infinite loop as we did in lecture)
+The philosophers pick up the chopsticks in any order, not lowest-numbered first (which we did in lecture).
+In order to eat, a philosopher must get permission from a host which executes in its own goroutine.
+The host allows no more than 2 philosophers to eat concurrently.
+Each philosopher is numbered, 1 through 5.
+When a philosopher starts eating (after it has obtained necessary locks) it prints “starting to eat <number>” on a line by itself, where <number> is the number of the philosopher.
+When a philosopher finishes eating (before it has released its locks) it prints “finishing eating <number>” on a line by itself, where <number> is the number of the philosopher.
+
+*/
+package main
+
+import (
+	"hash/fnv"
+	"log"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+// Number of philosophers is simply the length of this list.
+var ph = []string{"Mark", "Russell", "Rocky", "Haris", "Root"}
+
+const hunger = 3                // Number of times each philosopher eats
+const think = time.Second / 100 // Mean think time
+const eat = time.Second / 100   // Mean eat time
+
+var fmt = log.New(os.Stdout, "", 0)
+
+var dining sync.WaitGroup
+
+func diningProblem(phName string, dominantHand, otherHand *sync.Mutex) {
+	fmt.Println(phName, "Seated")
+	h := fnv.New64a()
+	h.Write([]byte(phName))
+	rg := rand.New(rand.NewSource(int64(h.Sum64())))
+	rSleep := func(t time.Duration) {
+		time.Sleep(t/2 + time.Duration(rg.Int63n(int64(t))))
+	}
+	for h := hunger; h > 0; h-- {
+		fmt.Println(phName, "Hungry")
+		dominantHand.Lock() // pick up forks
+		otherHand.Lock()
+		fmt.Println(phName, "Eating")
+		rSleep(eat)
+		dominantHand.Unlock() // put down forks
+		otherHand.Unlock()
+		fmt.Println(phName, "Thinking")
+		rSleep(think)
+	}
+	fmt.Println(phName, "Satisfied")
+	dining.Done()
+	fmt.Println(phName, "Left the table")
+}
+
+func main() {
+	fmt.Println("Table empty")
+	dining.Add(5)
+	fork0 := &sync.Mutex{}
+	forkLeft := fork0
+	for i := 1; i < len(ph); i++ {
+		forkRight := &sync.Mutex{}
+		go diningProblem(ph[i], forkLeft, forkRight)
+		forkLeft = forkRight
+	}
+	go diningProblem(ph[0], fork0, forkLeft)
+	dining.Wait() // wait for philosphers to finish
+	fmt.Println("Table empty")
+}
